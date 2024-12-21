f811e65b1936d28fdb463493b1e1123087a58739
RobotKava345 <elonmusk9933@gmail.com>
2024-12-21 18:02:26 +0200

    не відкриття папок і файлів

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from PIL import Image, ImageFilter, ImageEnhance
 from PyQt5 import QtWidgets, uic
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QMainWindow, QMessageBox, QListWidgetItem
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QListWidgetItem, QFileDialog
 from qt_material import apply_stylesheet
 import sys
+import os
 
 
 
@@ -49,9 +49,37 @@ class ImageEditor(QtWidgets.QMainWindow):
         self.original = None
         self.save_path = 'edited/'
         self.ui = Ui()
+        self.connects()
         self.ui.show()
         
     
+    def connects(self):
+        self.ui.folder_btn.clicked.connect(self.open_folder)
+        self.ui.open_folder.triggered.connect(self.open_folder)
+        self.ui.open_file.triggered.connect(self.open_file)
+
+    def get_images(self):
+        self.folder_images = []
+        if self.workdir:
+            file_names = os.listdir(self.workdir)
+            for file in file_names:
+                if file.endswith(".png") or file.endswith(".jpg") or file.endswith(".jpeg"):
+                    self.folder_images.append(file)
+
+
+    def open_folder(self):
+        self.workdir = QFileDialog.getExistingDirectory()
+        if self.workdir:
+            self.get_images()
+            self.ui.image_list.addItems(self.folder_images)
+        
+    def open_file(self):
+        file_path, _ = QFileDialog.getOpenFileName(self.ui, "Виберіть фото","", "Зображення (*.png *.jpg *.jpeg)")    
+        if file_path:
+            self.open(file_path)
+            print(self.image.filename)
+
+
     def open(self, filename):
         self.image = Image.open(filename)
         self.original = self.image
