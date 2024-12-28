add7d485d544b27fb340bd9fcd953922d2a00092
RobotKava345 <elonmusk9933@gmail.com>
2024-12-28 16:46:51 +0200

    Working code

--- main.py
+++ main.py
@@ -1,57 +1,34 @@
 from PIL import Image, ImageFilter, ImageEnhance
 from PyQt5 import QtWidgets, uic
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QListWidgetItem, QFileDialog
-from qt_material import apply_stylesheet
 import sys
 import os
 
+from PyQt5.QtWidgets import QApplication, QFileDialog
+from qt_material import apply_stylesheet
+#     # bright_obj = ImageEnhance.Brightness(img) # підсилювач яскравості
+#     # bright_img = bright_obj.enhance(2) # збільшуємо яскравість до 2
+#     # bright_img.show()
 
+#     contrast_img = ImageEnhance.Contrast(
+#         img).enhance(2)  # підсилювач контрастності
+#     contrast_img.show()
 
-#with Image.open("silas-schneider-BBoq3MaPNoM-unsplash.jpg") as img:
-    #print("Розмір:", img.size)
-    #print("Формат:", img.format)
-    #print("Тип:", img.mode)
-
-    #black_img = img.convert("L") # перетвор на ЧБ
-    #black_img.show()
-    #name = img.filename.split(".")[0]
-    #black_img.save(name+"_black.jpg")
-    #blur_image = img.filter(ImageFilter.BLUR)
-    #blur_image.save(name+"_blur.jpg")
-
-    #rotate_img = img.transpose(Image.ROTATE_90)
-    #rotate_img.save("rotate.jpg")
-    #rotate_img.show()
-    #sharpen_image = img.filter(ImageFilter.SHARPEN)
-    #sharpen_image.save("sharpen.jpg")
-
-    #bright_obj = ImageEnhance.Brightness(img) #підсилювач яскр
-    #bright_img = bright_obj.enhance(2) # підсил яскр до 2
-    #bright_img.show()
-
-    #contraster = ImageEnhance.Contrast(img)
-    #contrast_img = contraster.enhance(2)
-    #contrast_img.show()
-
-    #color_img = ImageEnhance.Color(img).enhance(2) #Насиченість кольору
-    #color_img.show()
-
+#     color_img = ImageEnhance.Color(img).enhance(2)  # насиченість кольору
+#     color_img.show()
 
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
         uic.loadUi('window.ui', self)
-        
 
-class ImageEditor(QtWidgets.QMainWindow):
-    def __init__(self):
-        self.image = None
+class ImageEditor():
+    def __init__(self,):
         self.original = None
+        self.image = None
         self.save_path = 'edited/'
         self.ui = Ui()
         self.connects()
         self.ui.show()
-        
     
     def connects(self):
         self.ui.folder_btn.clicked.connect(self.open_folder)
@@ -61,42 +38,40 @@ class ImageEditor(QtWidgets.QMainWindow):
     def get_images(self):
         self.folder_images = []
         if self.workdir:
-            file_names = os.listdir(self.workdir)
-            for file in file_names:
+            filenames = os.listdir(self.workdir)
+            for file in filenames:
                 if file.endswith(".png") or file.endswith(".jpg") or file.endswith(".jpeg"):
                     self.folder_images.append(file)
 
-
     def open_folder(self):
-        print("12121")
         self.workdir = QFileDialog.getExistingDirectory()
         if self.workdir:
             self.get_images()
             self.ui.image_list.addItems(self.folder_images)
-        
+
     def open_file(self):
-        file_path, _ = QFileDialog.getOpenFileName(self.ui, "Виберіть фото","", "Зображення (*.png *.jpg *.jpeg)")    
+        file_path, _ = QFileDialog.getOpenFileName(self.ui,
+                                        "Виберіть фото", "", "Зображення (*.png *.jpg *.jpeg)")
         if file_path:
             self.open(file_path)
             print(self.image.filename)
-
 
     def open(self, filename):
         self.image = Image.open(filename)
         self.original = self.image
 
     def do_black_white(self):
-        self.image = self.image.convert("L")
+        self.image = self.image.convert("L")  # перетворити на чорно-біле
 
     def do_blur(self):
         self.image = self.image.filter(ImageFilter.BLUR)
-
+    
     def rotate_90(self):
-        self.image = self.image.transpose(Image.ROTATE_90)
+        self.image = self.image.transpose(Image.ROTATE_90) #поворот
 
     def sharpen(self):
-        self.image = self.image.filter(ImageFilter.SHARPEN)
-
+        self.image = self.image.filter(ImageFilter.SHARPEN) #чіткість
+    
 
 app = QApplication([])
 editor = ImageEditor()
